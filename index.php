a1d7ec07c0a19e157acc967bce6aac5abbe2e08b
mamazu <maxo.pech@gmail.com>
2017-08-10 15:42:05 +0200

    Fleshed out the web page

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
- 
+<html>
+<head>
+    <title>Sorting visualizer</title>
+    <link rel="stylesheet" href="stylesheets/index.css" type="text/css">
+</head>
+<body>
+<div id="content">
+    <h1>Der B24 Super sorter</h1>
+    <form action="sort.php">
+        <fieldset>
+            <legend> Sorting algorithm</legend>
+            <select name="algorithm" title="Sorting algorithm">
+                <option value="bubble">bubble sort</option>
+            </select>
+        </fieldset>
+        <fieldset>
+            <legend>Numbers to sort</legend>
+            <textarea name="numbers" cols="30" rows="10" title="Numbers to sort">One number per line</textarea>
+        </fieldset>
+        <fieldset>
+            <legend>Sort or discard</legend>
+            <button type="submit">Sort</button>
+            <button type="reset">Discard</button>
+        </fieldset>
+    </form>
+</div>
+</body>
+</html>
